afb4740a7509d0a9b731cef4852e3b3c3b8b30be
Terrance Heldman <jivemasta@gmail.com>
2024-12-01 15:37:55 -0500

    Cleanup + refactor

--- AdventOfCode2024/DayOne/DayOne.cs
+++ AdventOfCode2024/DayOne/DayOne.cs
@@ -1,7 +1,12 @@
-﻿namespace AdventOfCode2024.DayOne
+﻿using AdventOfCode2024.Extensions;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.DayOne
 {
-    public class DayOne
+    public partial class DayOne
     {
+        private static int DistanceFunction(int left, int right) => Math.Abs(left - right);
+
         public static int PartOne()
         {
             (List<int> leftList, List<int> rightList) = LoadLists(@"DayOne\DayOneInput.txt");
@@ -16,32 +21,19 @@
 
         public static int CalculateDistance(List<int> leftList, List<int> rightList)
         {
-            leftList.Sort();
-            rightList.Sort();
-
-            int i = 0;
-            int result = 0;
-            foreach (int item in leftList)
-            {
-                result += Math.Abs(item - rightList[i]);
-                i++;
-            }
-
-            return result;
+            // Zip the two lists together using the distance function.
+            return leftList.Order().Zip(rightList.Order(), DistanceFunction).Sum();
         }
 
-        public static (List<int>, List<int>) LoadLists(string fileName)
+        public static (List<int> LeftList, List<int> RightList) LoadLists(string fileName)
         {
             List<int> leftList = [];
             List<int> rightList = [];
 
-            string[] lines = File.ReadAllLines(fileName);
-
-            foreach (string line in lines)
+            foreach (string line in File.ReadLines(fileName))
             {
-                IEnumerable<int> values = line.Split("   ").Select(v => Convert.ToInt32(v));
-                leftList.Add(values.First());
-                rightList.Add(values.Last());
+                leftList.Add(Convert.ToInt32(InputListsSplit().Match(line).Groups["left"].Value));
+                rightList.Add(Convert.ToInt32(InputListsSplit().Match(line).Groups["right"].Value));
             }
 
             return (leftList, rightList);
@@ -51,13 +43,24 @@
         {
             int result = 0;
 
-            foreach (int item in leftList)
+            // Get the count for each distinct value in both lists
+            Dictionary<int, int> leftListCounts = leftList.GetInstanceCount();
+            Dictionary<int, int> rightListCounts = rightList.GetInstanceCount();
+
+            // Since we know how many times the value appears in the left and right list, we just have to
+            // multiply the the count in both lists together, then multiply by the key value
+            foreach (int key in leftListCounts.Keys)
             {
-                int count = rightList.Where(v => v == item).Count();
-                result += item * count;
+                if (rightListCounts.TryGetValue(key, out int rightlistCount))
+                {
+                    result += rightlistCount * leftListCounts[key] * key;
+                }
             }
 
             return result;
         }
+
+        [GeneratedRegex(@"(?<left>\d+)\s+(?<right>\d+)")]
+        private static partial Regex InputListsSplit();
     }
 }
---
+++ AdventOfCode2024/Extensions/UtilityExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode2024.Extensions
+{
+    public static class UtilityExtensions
+    {
+        /// <summary>
+        /// Gets a index/value tuple of the items in a list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <returns></returns>
+        public static IEnumerable<(int Index, T Value)> Enumerate<T>(this IEnumerable<T> collection)
+        {
+            return collection.Select((i, index) => (index, i));
+        }
+
+        public static Dictionary<T, int> GetInstanceCount<T>(this IEnumerable<T> collection) where T : notnull
+        {
+            Dictionary<T, int> keyCount = [];
+            foreach (T item in collection)
+            {
+                keyCount[item] = keyCount.GetValueOrDefault(item) + 1;
+            }
+
+            return keyCount;
+        }
+    }
+}
