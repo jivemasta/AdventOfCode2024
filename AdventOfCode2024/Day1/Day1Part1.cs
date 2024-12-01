37bb2f3db1d2dc78ef4645e60d01497d19b30a9e
Terrance Heldman <jivemasta@gmail.com>
2024-12-01 12:38:09 -0500

    Add project files.

---
+++ AdventOfCode2024/Day1/Day1Part1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day1
+{
+    internal class Day1Part1
+    {
+
+    }
+}
---
+++ AdventOfCodes2024Tests/DayOneTests.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCodes2024Tests
+{
+    [TestClass]
+    public sealed class DayOneTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
