1a10d1f2324362ba1dc9d9b3a777c03b390d9091
heldmtj <terrence_j_heldman@whirlpool.com>
2024-12-02 14:33:26 -0500

    DayTwo Part One

--- AdventOfCode2024/DayTwo/DayTwo.cs
+++ AdventOfCode2024/DayTwo/DayTwo.cs
@@ -1,14 +1,72 @@
-﻿namespace AdventOfCode2024;
+﻿using System.Text.RegularExpressions;
+using AdventOfCode2024.Extensions;
 
-public class DayTwo
+namespace AdventOfCode2024;
+
+public partial class DayTwo
 {
-    public static string PartOne()
+    public enum Direction
     {
-        throw new NotImplementedException();
+        Up,
+        Down,
+        None
+    }
+
+    public static int PartOne()
+    {
+        return LoadReadings(@"DayTwo\DayTwoInput.txt")
+            .Select(r => SafetyCheck(r.ToList()))
+            .Where(c => c == true).Count();
     }
 
     public static string PartTwo()
     {
         throw new NotImplementedException();
     }
+
+    public static bool SafetyCheck(List<int> numberList)
+    {
+        Direction direction = GetDirection(numberList[0], numberList[1]);
+        foreach ((int Index, int Value) i in numberList.Enumerate())
+        {
+            // break out on last value of list
+            if (i.Index + 1 == numberList.Count)
+                break;
+
+            if (GetDirection(i.Value, numberList[i.Index + 1]) != direction)
+                return false;
+
+            if (Math.Abs(i.Value - numberList[i.Index + 1]) > 3)
+                return false;
+        }
+        return true;
+    }
+
+    public static bool SafetyCheckDamper(List<int> numberList)
+    {
+        throw new NotImplementedException();
+    }
+
+    private static Direction GetDirection(int numberOne, int numberTwo)
+    {
+        return (numberOne - numberTwo) switch
+        {
+            0 => Direction.None,
+            < 0 => Direction.Up,
+            > 0 => Direction.Down
+        };
+    }
+
+    public static IEnumerable<IEnumerable<int>> LoadReadings(string filename)
+    {
+        return File
+            .ReadLines(filename)
+            .Select(line => readingRegex()
+                            .Matches(line)
+                            .Select(m => Convert.ToInt32(m.Value))
+                            );
+    }
+
+    [GeneratedRegex(@"(\d+)")]
+    private static partial Regex readingRegex();
 }
--- AdventOfCodes2024Tests/DayTwoTests.cs
+++ AdventOfCodes2024Tests/DayTwoTests.cs
@@ -6,19 +6,46 @@
         [TestMethod]
         public void PartOneSample()
         {
-            Assert.Fail();
+            IEnumerable<IEnumerable<int>> readings = DayTwo.LoadReadings(@"DayTwo\DayTwoSampleInput.txt");
+            int results = readings.Select(r => DayTwo.SafetyCheck(r.ToList())).Where(c => c == true).Count();
+            Assert.AreEqual(2, results);
+        }
+
+        [TestMethod]
+        public void SafetyCheckSafe()
+        {
+            Assert.IsTrue(DayTwo.SafetyCheck([7, 6, 4, 2, 1]));
+            Assert.IsTrue(DayTwo.SafetyCheck([1, 3, 6, 7, 9]));
+        }
+
+        [TestMethod]
+        public void SafetyCheckUnsafe()
+        {
+            Assert.IsFalse(DayTwo.SafetyCheck([1, 2, 7, 8, 9]));
+            Assert.IsFalse(DayTwo.SafetyCheck([9, 7, 6, 2, 1]));
+            Assert.IsFalse(DayTwo.SafetyCheck([1, 3, 2, 4, 5]));
+            Assert.IsFalse(DayTwo.SafetyCheck([8, 6, 4, 4, 1]));
         }
 
         [TestMethod]
         public void PartOneTest()
         {
-            Assert.Fail();
+            DayTwo.PartOne();
         }
 
         [TestMethod]
         public void PartTwoSample()
         {
-            Assert.Fail();
+            IEnumerable<IEnumerable<int>> readings = DayTwo.LoadReadings(@"DayTwo\DayTwoSampleInput.txt");
+            int results = readings.Select(r => DayTwo.SafetyCheckDamper(r.ToList())).Where(c => c == true).Count();
+            Assert.AreEqual(4, results);
+        }
+
+        [TestMethod]
+        public void DamperChecks()
+        {
+            Assert.IsTrue(DayTwo.SafetyCheckDamper([7, 6, 4, 2, 1]));
+            Assert.IsFalse(DayTwo.SafetyCheckDamper([1, 2, 7, 8, 9]));
         }
 
         [TestMethod]
